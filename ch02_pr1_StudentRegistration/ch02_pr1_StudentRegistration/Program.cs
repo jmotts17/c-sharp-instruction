ed76cfc6b27d7f629121f551a969d396f79083f2
Josh Motta <joshua.a.motta@gmail.com>
2020-11-30 20:54:47 -0500

    init commit

---
+++ ch02_pr1_StudentRegistration/ch02_pr1_StudentRegistration/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ch02_pr1_StudentRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Student Registration Form\n");
+            
+            Console.Write("Enter first name: ");
+            String firstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            String lastName = Console.ReadLine();
+            Console.Write("Enter year of birth: ");
+            String yearOfBirth = Console.ReadLine();
+
+            Console.WriteLine("\nWelcome " + firstName + " " + lastName + "!");
+            Console.WriteLine("Your registration is complete.");
+            Console.WriteLine("Your temporary password is: " + firstName + "*" + yearOfBirth);
+        }
+    }
+}
