ba60c100c17b84192c3b8201d8086cef189349a7
Josh Motta <joshua.a.motta@gmail.com>
2020-11-30 20:55:06 -0500

    init commit

---
+++ ch02_pr2_GradeConverter/ch02_pr2_GradeConverter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ch02_pr2_GradeConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Letter Grade Converter");
+
+            String response;
+
+            do
+            {
+                Console.Write("\nEnter numerical grade: ");
+                int numGrade = Int32.Parse(Console.ReadLine());
+
+                if (numGrade <= 100 && numGrade >= 88)
+                {
+                    Console.WriteLine("Letter Grade: A");
+                }
+                else if (numGrade <= 100 && numGrade >= 80)
+                {
+                    Console.WriteLine("Letter Grade: B");
+                }
+                else if (numGrade <= 100 && numGrade >= 66)
+                {
+                    Console.WriteLine("Letter Grade: C");
+                }
+                else if (numGrade <= 100 && numGrade >= 60)
+                {
+                    Console.WriteLine("Letter Grade: D");
+                }
+                else if (numGrade < 60 && numGrade >= 0)
+                {
+                    Console.WriteLine("Letter Grade: F");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number.");
+                }
+
+                Console.Write("\nContinue? (y/n): ");
+                response = Console.ReadLine();
+            } while (response.Equals("y") || response.Equals("Y"));
+        }
+    }
+}
