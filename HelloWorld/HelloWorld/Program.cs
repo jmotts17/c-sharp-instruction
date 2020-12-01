b533939d896a8e6c5701872c8646fea0e5277fac
Josh Motta <joshua.a.motta@gmail.com>
2020-11-30 20:53:23 -0500

    init commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a single line comment
+
+            /*
+             * This is a block comment 
+            */
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your first name: ");
+            String firstName = Console.ReadLine();
+            Console.Write("Hello,");
+            Console.WriteLine(" " + firstName + "!");
+        }
+    }
+}
