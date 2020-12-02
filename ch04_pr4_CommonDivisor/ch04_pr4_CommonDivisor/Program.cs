ccaa27a4c5993f7d61a0d4de01684884ce2582cd
Josh Motta <joshua.a.motta@gmail.com>
2020-12-02 18:52:37 -0500

    init commit

---
+++ ch04_pr4_CommonDivisor/ch04_pr4_CommonDivisor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ch04_pr4_CommonDivisor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output welcome message
+            Console.WriteLine("Greatest Common Divisor Finder");
+
+            // Variable declaration
+            String response = "";
+
+            do
+            {
+                Console.Write("\nEnter first number: ");
+                int num1 = Int32.Parse(Console.ReadLine());
+                Console.Write("Enter second number: ");
+                int num2 = Int32.Parse(Console.ReadLine());
+
+                // Calculate GCD
+                while (num1 != num2)
+                {
+                    if (num1 > num2)
+                    {
+                        num1 = num1 - num2;
+                    }
+                    else
+                    {
+                        num2 = num2 - num1;
+                    }
+                }
+
+                // Output results
+                Console.WriteLine("Greatest Common Divisor: " + num2);
+
+                // Prompt user to continue
+                Console.Write("\nContinue? (y/n): ");
+                response = Console.ReadLine();
+            } while (response.Equals("y") || response.Equals("Y"));
+
+        }
+    }
+}
