e40a0607d3a136ee588f5f75bdcd92d2466f6eea
Josh Motta <joshua.a.motta@gmail.com>
2020-12-01 21:41:20 -0500

    init commit

---
+++ ch03_pr1_TemperatureConverter/ch03_pr1_TemperatureConverter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ch03_pr1_TemperatureConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output welcome message
+            Console.WriteLine("Welcome to the Temperature Converter");
+
+            // Variable Declaration
+            String response;
+
+            do
+            {
+                // Prompt user for input
+                Console.Write("\nEnter degrees in Fahrenheit: ");
+                double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+                // Calculate & Output Results
+                double celsius = (fahrenheit - 32) * 5 / 9;
+                Console.WriteLine("Degrees in Celsius: " + Math.Round(celsius, 2));
+
+                // Prompt user to continue
+                Console.Write("\nContinue? (y/n): ");
+                response = Console.ReadLine();
+            } while (response.Equals("y") || response.Equals("Y"));
+        }
+    }
+}
