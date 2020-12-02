141697d52d3fc9f9fcb9c8f7568242af748be52e
Josh Motta <joshua.a.motta@gmail.com>
2020-12-02 18:51:53 -0500

    init commit

---
+++ ch03-for-loop/ch03-for-loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ch03_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Loops");
+            // index for loop that will print "hello" n times
+            // n is an int entered by the user
+
+            Console.Write("Enter a whole number: ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Hello " + i);
+            }
+        }
+    }
+}
