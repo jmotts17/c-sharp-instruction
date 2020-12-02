d7e38c1bc75277a4257775684c04ecddae9a405a
Josh Motta <joshua.a.motta@gmail.com>
2020-12-01 22:02:10 -0500

    init commit

---
+++ ch03_pr3_InterestCalculator/ch03_pr3_InterestCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ch03_pr3_InterestCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output welcome message
+            Console.WriteLine("Welcome to the Interest Calculator");
+
+            // Variable declaration
+            String response;
+
+            do
+            {
+                // Prompt user for input
+                Console.Write("\nEnter loan amount:   ");
+                Double loanAmount = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter interest rate: ");
+                Double interestRate = Convert.ToDouble(Console.ReadLine());
+
+                // Calculate results
+                Double interest = Math.Round(loanAmount * interestRate, 2, MidpointRounding.AwayFromZero);
+
+                // Output results
+                Console.WriteLine("\nLoan amount: \t" + loanAmount.ToString("C"));
+                Console.WriteLine("Interest Rate: \t" + interestRate.ToString("P"));
+                Console.WriteLine("Interest: \t" + interest.ToString("C"));
+
+                // Prompt user to continue
+                Console.Write("\nContinue? (y/n): ");
+                response = Console.ReadLine();
+            } while (response.Equals("Y") || response.Equals("y"));
+
+
+        }
+    }
+}
