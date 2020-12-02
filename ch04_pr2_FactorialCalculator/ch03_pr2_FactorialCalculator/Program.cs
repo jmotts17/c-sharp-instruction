d47d005fe42482fbaf0e9647faa8a1c5b6e1eb88
Josh Motta <joshua.a.motta@gmail.com>
2020-12-02 18:52:27 -0500

    init commit

---
+++ ch04_pr2_FactorialCalculator/ch03_pr2_FactorialCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ch04_pr2_FactorialCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output welcome message
+            Console.WriteLine("Welcome to the Factorial Calculator");
+
+            // Variable declaration
+            String response = "";
+
+            do
+            {
+                // Prompt user for input
+                Console.Write("\nEnter an integer that's greater than 0 and less than 10: ");
+                int num = Int32.Parse(Console.ReadLine());
+
+                // Declare factorial variable
+                long factorial = 1;
+
+                // Calculate factorial
+                for (int i = 1; i <= num; i++)
+                {
+                    factorial *= i;
+                }
+
+                // Output results
+                Console.WriteLine("The factorial of " + num + " is " + factorial + ".");
+
+                // Prompt user to continue
+                Console.Write("\nContinue? (y/n): ");
+                response = Console.ReadLine();
+            } while (response.Equals("y") || response.Equals("Y"));
+        }
+    }
+}
