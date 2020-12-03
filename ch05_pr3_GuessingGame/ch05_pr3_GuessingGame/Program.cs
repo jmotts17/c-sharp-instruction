135e8b727759b1b740c85ffe44cad4220b27905d
Josh Motta <joshua.a.motta@gmail.com>
2020-12-02 21:12:12 -0500

    init commit

---
+++ ch05_pr3_GuessingGame/ch05_pr3_GuessingGame/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace ch05_pr3_GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output welcome message
+            OutputWelcomeMessage();
+
+            // Variable declaration
+            String response = "";
+            
+            // Random number reference
+            Random rand = new Random();
+
+            do
+            {
+                // Generate a random number
+                int randomNumber = rand.Next(100) + 1;
+                // Variable Declaration
+                int guess = 0;
+                int count = 0;
+
+                while (guess != randomNumber)
+                {
+                    // Prompt user for a guess
+                    guess = GetIntWithinRange("\nEnter number: ", 0, 100);
+                    // Increment user guess count
+                    count++;
+                    // Output user hints based on guess vs random number
+                    if (guess - randomNumber > 10)
+                    {
+                        Console.WriteLine("Way too high!");
+                    }
+                    else if (guess - randomNumber > 0)
+                    {
+                        Console.WriteLine("Too High!");
+                    }
+                    else if (randomNumber - guess > 10)
+                    {
+                        Console.WriteLine("Way too low!");
+                    } else if (randomNumber - guess > 0)
+                    {
+                        Console.WriteLine("Too low!");
+                    } 
+                }
+                // Output results + special message
+                Console.WriteLine("You got it in " + count + " tries.");
+                OutputSpecialMessage(count);
+
+                // Prompt user to continue
+                response = GetString("\nContinue? (y/n): ");
+            } while (response.Equals("Y") || response.Equals("y"));
+
+
+        }
+
+        public static void OutputWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the Guess the Number Game");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("I'm thinking of a number from 1 to 100");
+            Console.WriteLine("Try to guess it.");
+        }
+
+        public static int GetIntWithinRange(String prompt, int min, int max)
+        {
+            int num = 0;
+            Boolean success = false;
+            while (!success)
+            {
+                try
+                {
+                    Console.Write(prompt);
+                    num = int.Parse(Console.ReadLine());
+                    if (num > min && num <= max)
+                    {
+                        success = true;
+                    } else
+                    {
+                        Console.WriteLine("Invalid entry. Enter a number between 1 and 100.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid entry. Enter a number between 1 and 100.");
+                }
+            }
+            return num;
+        }
+
+        public static string GetString(String prompt)
+        {
+            String response = "";
+            Boolean success = false;
+            while (!success)
+            {
+                Console.Write(prompt);
+                response = Console.ReadLine();
+                if (response.Equals("y") || response.Equals("n")) {
+                    success = true;
+                } else
+                {
+                    Console.WriteLine("Invalid entry. Please enter 'y' or 'n'.");
+                }
+            }
+
+            return response;
+        }
+
+        public static void OutputSpecialMessage(int count)
+        {
+            if(count <= 3)
+            {
+                Console.WriteLine("Great work! You are a mathematical wizard.");
+            } else if (count > 3 && count <=6)
+            {
+                Console.WriteLine("Not too bad! You've got some potential.");
+            } else
+            {
+                Console.WriteLine("What took you so long? Maybe you should take some lessons.");
+            }
+        }
+    }
+}
