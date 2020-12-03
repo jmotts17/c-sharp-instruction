0ec0d9aa525aedaa005332c8700804a317e6d260
Josh Motta <joshua.a.motta@gmail.com>
2020-12-02 19:27:05 -0500

    init commit

---
+++ ch05-optional-arguments/ch05-optional-arguments/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ch05_optional_arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Optional args!\n");
+            int nb1 = 22;
+            int nb2 = 44;
+
+            Console.WriteLine("Sum is "+ Sum(nb1, nb2));
+
+            Console.WriteLine("\nBye!");
+        }
+
+        static int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
