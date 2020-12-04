3ba0579c74cd15ccf9f882b09f1894a90539ebe6
Josh Motta <joshua.a.motta@gmail.com>
2020-12-03 22:58:44 -0500

    init commit

---
+++ ch05_MethodCalculator/ch05_MethodCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ch05_MethodCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Method Calculator");
+        }
+    }
+}
