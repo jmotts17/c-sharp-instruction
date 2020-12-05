8b2f7d8238d700302f00a636e6fd034bd73f98a6
Josh Motta <joshua.a.motta@gmail.com>
2020-12-05 11:09:12 -0500

    init commit

---
+++ ch11-ArraysDemo/ch11-ArraysDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ch11_ArraysDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's do some arrays!!");
+
+            String[] names = { "Chris", "Josh", "Derek", "Monroe" };
+            Console.WriteLine("\nElement at position 1: " + names[1]);
+
+            Console.WriteLine("\nIndexed For Loop");
+
+            // indexed for loop
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("Element at position " + i + ": " + names[i]);
+            }
+
+            Console.WriteLine("\nFor Each Loop");
+
+            // for each loop
+            foreach (String name in names)
+            {
+                Console.WriteLine("Name: " + name);
+            }
+
+            Console.WriteLine("\nBye");
+        }
+    }
+}
---
+++ ch12-ListAndDictionary/ch12-ListAndDictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ch12_ListAndDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create some list and a dictionary");
+
+            // a list
+            List<double> prices = new List<double>();
+            prices.Add(11.11);
+            prices.Add(5.99);
+            prices.Add(250.25);
+
+            Console.WriteLine("\n" + prices[1]);
+
+            Console.WriteLine("\nFor Loop");
+            for(int i = 0; i < prices.Count; i++)
+            {
+                Console.WriteLine("Price " + i + ": " + prices[i]);
+            }
+
+            Console.WriteLine("\nFor Each Loop");
+            foreach (double price in prices)
+            {
+                Console.WriteLine(price);
+            }
+
+            Console.WriteLine("\nBye");
+        }
+    }
+}
