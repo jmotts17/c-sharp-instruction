bfb7d2b4f6ffacd05e9eb7ab44bd857263d23155
Josh Motta <joshua.a.motta@gmail.com>
2020-12-05 13:09:52 -0500

    init commit

---
+++ ch12_pr3_MovieList/ch12_pr3_MovieList/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ch12_pr3_MovieList
+{
+    class Movie
+    {
+        public String title { get; set; }
+        public String category { get; set; }
+
+        public Movie()
+        {
+
+        }
+
+        public Movie(string title, string category)
+        {
+            this.title = title;
+            this.category = category;
+        }
+    }
+}
---
+++ ch12_pr3_MovieList/ch12_pr3_MovieList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ch12_pr3_MovieList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Movie List Application");
+
+            String response;
+
+            do
+            {
+                CategoryMenu();
+                int selection = int.Parse(Console.ReadLine());
+
+                Console.Write("Continue? (y/n): ");
+                response = Console.ReadLine();
+            } while (response.Equals("y"));
+
+        }
+
+        public static void CategoryMenu()
+        {
+            Console.WriteLine("\nMovie Categories");
+            Console.WriteLine("1. Animated");
+            Console.WriteLine("2. Drama");
+            Console.WriteLine("3. Horror");
+            Console.WriteLine("4. Scifi");
+            Console.Write("\nEnter a category number: ");
+        }
+    }
+}
