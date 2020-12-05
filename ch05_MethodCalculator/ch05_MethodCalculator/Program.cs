a4bfa28668ca620f45bb588abbaf534f380f4b9f
Josh Motta <joshua.a.motta@gmail.com>
2020-12-04 23:09:06 -0500

    Added methods

--- ch05_MethodCalculator/ch05_MethodCalculator/Program.cs
+++ ch05_MethodCalculator/ch05_MethodCalculator/Program.cs
@@ -7,6 +7,26 @@ namespace ch05_MethodCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Method Calculator");
+
+            String response;
+            double result = 0;
+        }
+
+        public static double Sum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public static double Difference(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public static double Product(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public static double Quotient(double num1, double num2)
+        {
+            return num1 / num2;
         }
     }
 }
